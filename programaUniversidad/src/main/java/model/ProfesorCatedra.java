095534f5b04b46396c8c8d3f50a959f6d6fd47ef
eliza-maker <elcuve08@gmail.com>
2025-09-30 21:47:56 -0500

    update1
    Se crearon las enumeraciones y las clase profesor y sus hijas quedaron finalizadas

--- programaUniversidad/src/main/java/model/ProfesorCatedra.java
+++ programaUniversidad/src/main/java/model/ProfesorCatedra.java
@@ -1,7 +1,28 @@
 package model;
 
-public class ProfesorCatedra extends Materia{
-    public ProfesorCatedra(String codigo, String nombre, Double numHoras, int creditos) {
-        super(codigo, nombre, numHoras, creditos);
+public class ProfesorCatedra extends Profesor{
+    protected int horaContrato;
+    protected boolean trabajoExtra;
+
+    public ProfesorCatedra(String nombre, String identificador, String titulo, byte aniosXP, int horaContrato, boolean trabajoExtra) {
+        super(nombre, identificador, titulo, aniosXP);
+        this.horaContrato = horaContrato;
+        this.trabajoExtra = trabajoExtra;
+    }
+
+    public int getHoraContrato() {
+        return horaContrato;
+    }
+
+    public void setHoraContrato(int horaContrato) {
+        this.horaContrato = horaContrato;
+    }
+
+    public boolean isTrabajoExtra() {
+        return trabajoExtra;
+    }
+
+    public void setTrabajoExtra(boolean trabajoExtra) {
+        this.trabajoExtra = trabajoExtra;
     }
 }
